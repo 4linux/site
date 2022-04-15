e9acbff1296e06ff7b56ffc39df9b2e7a65d6e3e
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2022-04-15 17:13:05 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,5 +4,4 @@
 	<li><a href="organograma.php" title="Organograma"><img src="menu/stats.png" /></a></li>
 	<li><a href="funcionarios.php" title="Funcion&aacute;rios"><img src="menu/usuarios.png" /></a></li>
 	<li><a href="agenda.php" title="Calend&aacute;rio"><img src="menu/agenda.png" /></a></li>
-	<li><a href="backup.php" title="Backup"><img src="menu/backup.png" /></a></li>
 </ul>
